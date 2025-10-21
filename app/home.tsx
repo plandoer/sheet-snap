0a63567fd9ffa31f8edf4b9fbbb7d3d6cb44ca0b
yeminko <yeminko47@gmail.com>
2025-10-21 22:27:53 +0630

    Add owner field and update app configuration in app.json; add SafeAreaView import in Home component; update dependencies in package.json and package-lock.json

--- app/home.tsx
+++ app/home.tsx
@@ -1,7 +1,6 @@
 import { Stack } from "expo-router";
 import { useState } from "react";
 import {
-  SafeAreaView,
   ScrollView,
   StyleSheet,
   Text,
@@ -9,6 +8,7 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Home() {
   const [amount, setAmount] = useState("");
